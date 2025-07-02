a29a0c741d1344fa5ca11443352e6878f23a267d
orion3dnan2 <eng.mohamed.adnan.m@gmail.com>
2025-07-02 19:53:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from app import app
+from app import app, db
 import os
 
 if __name__ == "__main__":
+    # إنشاء الجداول إذا لم تكن موجودة
+    with app.app_context():
+        db.create_all()
+
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
